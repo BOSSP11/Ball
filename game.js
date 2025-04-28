3293f794147fd351645793f1fafe5bdf4ed3822c
BOSSP11 <markjameslegatic@gmail.com>
2025-04-29 01:23:36 +0800

    Update game.js

--- game.js
+++ game.js
@@ -50,7 +50,7 @@ function update() {
   ball.x += ball.velocityX;
   ball.y += ball.velocityY;
 
-  ball.velocityY += 0.30;
+  ball.velocityY += 0.25;
 
   if (ball.y + ball.radius > canvas.height) {
     gameOver();
